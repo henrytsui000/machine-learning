f633cc126e4c3f6ce4c4a13d57078f8d26316e26
henrytsui <henrytsui000@gmail.com>
2021-02-05 14:50:57 +0800

    hand_write number by DNN

--- DNN.py
+++ DNN.py
@@ -21,7 +21,7 @@ def load_data():  # categorical_crossentropy
     y_test = np_utils.to_categorical(y_test, 10)
     x_train = x_train
     x_test = x_test
-    x_test = np.random.normal(x_test)  # 加噪声
+    x_test = np.random.normal(x_test)  
     x_train = x_train / 255
     x_test = x_test / 255
 
@@ -29,18 +29,6 @@ def load_data():  # categorical_crossentropy
 
 
 if __name__ == '__main__':
-    '''
-    注意事项如下：
-    1、batch_size=100,epochs=20为宜，batch_size过大会导致loss下降曲线过于平滑而卡在local minima、saddle point或plateau处，batch_size过小会导致update次数过多，运算量太大，速度缓慢，但可以带来一定程度的准确率提高
-    2、hidden layer数量不要太多，不然可能会发生vanishing gradient(梯度消失)，一般两到三层为宜
-    3、如果layer数量太多，则千万不要使用sigmoid等缩减input影响的激活函数，应当选择ReLU、Maxout等近似线性的activation function(layer数量不多也应该选这两个)
-    4、每一个hidden layer所包含的neuron数量，五六百为宜
-    5、对于分类问题，loss function一定要使用cross entropy(categorical_crossentropy)，而不是mean square error(mse)
-    6、优化器optimizer一般选择adam，它综合了RMSProp和Momentum，同时考虑了过去的gradient、现在的gradient，以及上一次的惯性
-    7、如果testing data上准确率很低，training data上准确率比较高，可以考虑使用dropout，Keras的使用方式是在每一层hidden layer的后面加上一句model.add(Dropout(0.5))，其中0.5这个参数你自己定；注意，加了dropout之后在training set上的准确率会降低，但是在testing set上的准确率会提高，这是正常的
-    8、如果input是图片的pixel，注意对灰度值进行归一化，即除以255，使之处于0～1之间
-    9、最后的output最好同时输出在training set和testing set上的准确率，以便于对症下药
-    '''
     # load training data and testing data
     (x_train, y_train), (x_test, y_test) = load_data()
 
@@ -48,17 +36,21 @@ if __name__ == '__main__':
     model = Sequential()
 
     model.add(Dense(input_dim=28 * 28, units=500, activation='relu'))
+    # input an image have 28 * 28 pixel, and connect a layer having 500 neuron
     # model.add(Dropout(0.5))
     model.add(Dense(units=500, activation='relu'))
     # model.add(Dropout(0.5))
     model.add(Dense(units=10, activation='softmax'))
+    #output to 10 neuron be the output layer and use softmax to enhance the signal
 
     # set configurations
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
+    # use cross_entropy to decide a network is good, use gradient descent
 
     # train model
     model.fit(x_train, y_train, batch_size=100, epochs=20)
+    # actual training model, 100 data a group to train, and do 20 times
 
     # evaluate the model and output the accuracy
     result_train = model.evaluate(x_train, y_train)
